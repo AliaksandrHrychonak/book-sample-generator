29cfdbe498e2c1026f36f0d5c4fdc989d4c2c991
AliaksandrHrychonak <@icloud.com>
2025-03-28 04:56:11 +0300

    refactor(ui): add return type skeleton

--- src/shared/ui/skeleton/index.tsx
+++ src/shared/ui/skeleton/index.tsx
@@ -1,8 +1,8 @@
 import { cn } from '../../lib';
 
-import type { HTMLAttributes } from 'react';
+import type { HTMLAttributes, JSX } from 'react';
 
-const Skeleton = ({ className, ...props }: HTMLAttributes<HTMLDivElement>) => {
+const Skeleton = ({ className, ...props }: HTMLAttributes<HTMLDivElement>): JSX.Element => {
     return <div className={cn('animate-pulse rounded-md bg-primary/10', className)} {...props} />;
 };
 
