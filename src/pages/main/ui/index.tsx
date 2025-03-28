69534d4535cf6364a2bab149d53fb72e156cf083
AliaksandrHrychonak <@icloud.com>
2025-03-28 04:54:32 +0300

    feat(book-search): implement book list and search components

--- src/pages/main/ui/index.tsx
+++ src/pages/main/ui/index.tsx
@@ -1,10 +1,17 @@
+import { SearchBookStoreProvider } from '@entities/book';
+import { BookList } from '@widgets/book-list';
+import { BookSearchBar } from '@widgets/book-search-bar';
+
 import type { FC, JSX } from 'react';
 
 export const MainPage: FC = (): JSX.Element => {
     return (
-        <main className='grid min-h-svh pt-20 pr-20 pl-20 max-[900px]:pr-5 max-[900px]:pl-5'>
-            main
-        </main>
+        <SearchBookStoreProvider>
+            <main className='grid pt-20 pr-20 pl-20 max-[900px]:pr-5 max-[900px]:pl-5'>
+                <BookSearchBar />
+                <BookList />
+            </main>
+        </SearchBookStoreProvider>
     );
 };
 
